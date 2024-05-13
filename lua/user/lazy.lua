395d50b9fd19976914d995ac6f07f18a73519292
Elwardi <fadeli.elwardi@tu-darmstadt.de>
2024-05-13 12:45:45 +0200

    fix: update lazy configs

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,26 +1,30 @@
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system {
+	vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable",
 		lazypath,
-	}
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup {
+require("lazy").setup({
 	spec = LAZY_PLUGIN_SPEC,
+	git = {
+		log = { "-10" },
+	},
 	install = {
 		colorscheme = { "primer_dark", "habamax" },
 	},
 	ui = {
 		border = "rounded",
+		title = "Lazy Plugins",
 	},
 	change_detection = {
 		enabled = true,
 		notify = false,
 	},
-}
+})
