8f8cd279027b376b5e015030aa3ab0aea904d133
Elwardi <fadeli.elwardi@tu-darmstadt.de>
2024-05-13 12:43:50 +0200

    fix: update autocmds

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -1,19 +1,30 @@
 vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+    desc = "Huh?",
 	callback = function()
 		vim.cmd "quit"
 	end,
 })
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+    desc = "Highight when yanking",
 	callback = function()
-		vim.highlight.on_yank { higroup = "Visual", timeout = 40 }
+		vim.highlight.on_yank { higroup = "@comment.warning", timeout = 40 }
 	end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "gitcommit", "markdown", "NeogitCommitMessage" },
+    desc = "Set wrap and spell for some file types",
+	pattern = { "gitcommit", "markdown", "latex", "tex", "NeogitCommitMessage" },
 	callback = function()
 		vim.opt_local.wrap = true
 		vim.opt_local.spell = true
 	end,
 })
+
+
+--vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+--    desc = "fix cmp completion in visual mode",
+--	callback = function()
+--        require('cmp').setup.buffer({ enabled = vim.api.nvim_get_mode().mode == 'c' })
+--	end,
+--})
