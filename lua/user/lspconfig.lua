4f2d3bb1c83407021c22f051bff80440a9324900
Elwardi <fadeli.elwardi@tu-darmstadt.de>
2024-05-13 12:46:44 +0200

    fix: remove tailwindcss and use marksman for markdown

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -1,132 +1,135 @@
 local M = {
-	"neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig",
     event = { "BufReadPost", "BufNewFile" },
     cmd = { "LspInfo", "LspInstall", "LspUninstall" },
-	dependencies = {
-		{
-			"folke/neodev.nvim",
+    dependencies = {
+        {
+            "folke/neodev.nvim",
             "williamboman/mason.nvim",
             "williamboman/mason.nvim",
-		},
-	},
+        },
+    },
 }
 
 local function lsp_keymaps(bufnr)
-	local opts = { noremap = true, silent = true }
-	local keymap = vim.api.nvim_buf_set_keymap
-	keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+    local opts = { noremap = true, silent = true }
+    local keymap = vim.api.nvim_buf_set_keymap
+    keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 end
 
 M.on_attach = function(client, bufnr)
-	lsp_keymaps(bufnr)
+    lsp_keymaps(bufnr)
 end
 
 function M.common_capabilities()
-	local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-	if status_ok then
-		return cmp_nvim_lsp.default_capabilities()
-	end
+    local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+    if status_ok then
+        return cmp_nvim_lsp.default_capabilities()
+    end
 
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
-	capabilities.textDocument.completion.completionItem.resolveSupport = {
-		properties = {
-			"documentation",
-			"detail",
-			"additionalTextEdits",
-		},
-	}
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+            "documentation",
+            "detail",
+            "additionalTextEdits",
+        },
+    }
+    capabilities.textDocument.codeLens = { dynamicRegistration = false }
 
-	return capabilities
+    return capabilities
 end
 
 function M.config()
-	local lspconfig = require("lspconfig")
-	local icons = require("user.lspicons")
+    local lspconfig = require("lspconfig")
+    local icons = require("user.lspicons")
 
-	local servers = {
-		"clangd",
-		"lua_ls",
-		"cssls",
-		"html",
-		"tsserver",
-		"astro",
-		"pyright",
-		"bashls",
-		"jsonls",
-		"yamlls",
-		"tailwindcss",
-		"glsl_analyzer",
-		"foam_ls",
-		"rust_analyzer",
-	}
+    local servers = {
+        "clangd",
+        "lua_ls",
+        "cssls",
+        "html",
+        "tsserver",
+        "astro",
+        "pyright",
+        "bashls",
+        "jsonls",
+        "yamlls",
+        "glsl_analyzer",
+        "foam_ls",
+        "rust_analyzer",
+        "marksman",
+    }
 
-	local default_diagnostic_config = {
-		signs = {
-			active = true,
-			values = {
-				{ name = "DiagnosticSignError", text = icons.diagnostics.Error },
-				{ name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-				{ name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-				{ name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
-			},
-		},
-		virtual_text = false,
-		update_in_insert = false,
-		underline = true,
-		severity_sort = true,
-		float = {
-			focusable = true,
-			style = "minimal",
-			border = "rounded",
-			source = "always",
-			header = "",
-			prefix = "",
-		},
-	}
+    local default_diagnostic_config = {
+        signs = {
+            active = true,
+            values = {
+                { name = "DiagnosticSignError", text = icons.diagnostics.Error },
+                { name = "DiagnosticSignWarn",  text = icons.diagnostics.Warning },
+                { name = "DiagnosticSignHint",  text = icons.diagnostics.Hint },
+                { name = "DiagnosticSignInfo",  text = icons.diagnostics.Information },
+            },
+        },
+        virtual_text = false,
+        update_in_insert = false,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = true,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+        },
+    }
 
-	vim.diagnostic.config(default_diagnostic_config)
+    vim.diagnostic.config(default_diagnostic_config)
 
-	for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-	end
+    for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
+        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+    end
 
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-	--vim.lsp.handlers["textDocument/signatureHelp"] =
-	--	vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-	require("lspconfig.ui.windows").default_options.border = "rounded"
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+--function(_, _, result, _, bufnr)
+--            return nil
+--        end
 
-	for _, server in pairs(servers) do
-		local opts = {
-			on_attach = M.on_attach,
-			capabilities = M.common_capabilities(),
+    --vim.lsp.handlers["textDocument/signatureHelp"] =
+    --	vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+    require("lspconfig.ui.windows").default_options.border = "rounded"
+
+    for _, server in ipairs(lspconfig) do
+    end
+    for _, server in pairs(servers) do
+        local opts = {
+            on_attach = M.on_attach,
+            capabilities = M.common_capabilities(),
             handlers = vim.lsp.handlers,
-		}
+        }
 
-		local require_ok, settings = pcall(require, "user.lspsettings." .. server)
-		if require_ok then
-			opts = vim.tbl_deep_extend("force", settings, opts)
-		end
+        local require_ok, settings = pcall(require, "user.lspsettings." .. server)
+        if require_ok then
+            opts = vim.tbl_deep_extend("force", settings, opts)
+        end
 
-		if server == "lua_ls" then
-			require("neodev").setup({})
-		end
+        if server == "lua_ls" then
+            require("neodev").setup({})
+        end
 
-		if server == "clangd" then
+        if server == "clangd" then
             opts.cmd = { "clangd", "--offset-encoding=utf-16" }
-		end
+        end
 
-		--if server == "pyright" then
-		--	print(vim.inspect(opts))
-		--end
-
-		lspconfig[server].setup(opts)
-	end
+        lspconfig[server].setup(opts)
+    end
 end
 
 return M
