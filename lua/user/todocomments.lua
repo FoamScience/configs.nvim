3446c57bf5aae3e5560be622371e4df6159ded6a
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2024-05-31 11:19:06 +0200

    fix: add DEBUG to todos

--- lua/user/todocomments.lua
+++ lua/user/todocomments.lua
@@ -12,6 +12,7 @@ function M.config()
             HACK = { icon = " ", color = "warning", alt = { "hack", } },
             WARN = { icon = " ", color = "warning", alt = { "warn", } },
             TEST = { icon = "⏲ ", color = "test", alt = { "test", } },
+            DEBUG = { icon = " ", color = "warning", alt = { "test", } },
         },
         merge_keywords = false,
         search = {
