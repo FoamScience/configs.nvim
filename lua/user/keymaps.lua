03c3b35468fc516fd11cb8d4d9b0caf8861bf3de
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2024-11-11 22:28:56 +0100

    fix: improve buffer hopping with TAB

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,16 +1,44 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
+local function is_floating_window()
+    return vim.api.nvim_win_get_config(0).relative ~= ""
+end
 
 keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-whichkey_ok, _ = pcall(require, "whichkey")
+local whichkey_ok, _ = pcall(require, "whichkey")
 if whichkey_ok then
     keymap("n", "<C-Space>", "<cmd>WhichKey \\<space><cr>", opts)
 end
 keymap("n", "<C-i>", "<C-i>", opts)
 
 -- Buffer hopping
-keymap("n", "<Tab>", "<cmd>bn<cr>", opts)
-keymap("n", "<S-Tab>", "<cmd>bp<cr>", opts)
+local disabled_tab_hopping = {
+    help = true,
+    qf = true,
+    nofile = true,
+    fugitiveblame = true,
+    fugitive = true,
+    NeogitStatus = true,
+    fzf = true,
+    lazy = true,
+    mason = true,
+}
+keymap("n", "<Tab>", function()
+    if disabled_tab_hopping[vim.bo.filetype]
+        or is_floating_window()
+    then
+        return
+    end
+    vim.cmd("bn")
+end, opts)
+keymap("n", "<S-Tab>", function()
+    if disabled_tab_hopping[vim.bo.filetype]
+        or is_floating_window()
+    then
+        return
+    end
+    vim.cmd("bp")
+end, opts)
