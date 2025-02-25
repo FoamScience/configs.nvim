8495c4661e7b773b84fdf8b1d5c90717911c9b24
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2025-02-25 12:25:57 +0100

    fix: imagae and latex equation rendering

--- lua/user/optional/image.lua
+++ lua/user/optional/image.lua
@@ -10,10 +10,16 @@ M.config = function()
         processor = "magick_cli",
         integrations = {
             markdown = {
-                enabled = false,
+                resolve_image_path = function(doc_path, image_path, fallback)
+                    vim.print("image path: ", image_path)
+                    vim.print("doc path: ", doc_path)
+                    return fallback(doc_path, image_path)
+                end,
+                enabled = true,
                 clear_in_insert_mode = false,
                 download_remote_images = true,
-                only_render_image_at_cursor = false,
+                only_render_image_at_cursor = true,
+                floating_windows = true,
                 filetypes = { "markdown", "vimwiki" },
             },
             neorg = {
@@ -36,7 +42,7 @@ M.config = function()
         max_width_window_percentage = nil,
         max_height_window_percentage = 50,
         window_overlap_clear_enabled = false,
-        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "scrollview", "scrollview_sign", "" },
         editor_only_render_when_focused = false,
         tmux_show_only_in_active_window = false,
         hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
--- lua/user/render-markdown.lua
+++ lua/user/render-markdown.lua
@@ -17,6 +17,29 @@ M.config = function()
         code = {
             style = "full",
         },
+        latex = {
+            enabled = true,
+            render_modes = false,
+            converter = 'latex2text',
+            highlight = 'RenderMarkdownMath',
+            position = 'above',
+            top_pad = 0,
+            bottom_pad = 0,
+        },
+        checkbox = {
+            enabled = true,
+            position = 'inline',
+            unchecked = {
+                icon = '󰄱 ',
+                highlight = 'RenderMarkdownUnchecked',
+                scope_highlight = nil,
+            },
+            checked = {
+                icon = '󰱒 ',
+                highlight = 'RenderMarkdownChecked',
+                scope_highlight = nil,
+            },
+        },
     }
 end
 
