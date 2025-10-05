40706c33994da06fa3695ed8e8e9b8e5b0ca1731
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2025-10-05 17:19:22 +0200

    feat: add markdown ToC config

---
+++ lua/user/markdown-toc.lua
@@ -0,0 +1,37 @@
+local M = {
+    "FoamScience/markdown-toc.nvim",
+    ft = { "markdown", "rmd" },
+    branch = "no-nvim-clutter",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter", -- for efficient parsing
+        "nvim-telescope/telescope.nvim",   -- for picker UI
+    },
+    build = ":TSInstall markdown",
+    event = "VeryLazy",
+}
+
+M.config = function()
+    require("mtoc").setup({
+        debug = false,
+        headings = {
+            before_toc = false,
+            min_depth = 1,
+            max_depth = 4,
+        },
+        fences = {
+            start_text = {"mtoc-start", "mtoc-old-start"},
+            end_text = {"mtoc-end", "mtoc-old-end"},
+        },
+        auto_update = {
+            enabled = true,
+            events = { 'BufWritePre' },
+            pattern = '*.{md,mdown,mkd,mkdn,markdown,mdwn}',
+            suppress_pollution = true,
+        },
+        toc_list = {
+            numbered = false,
+        }
+    })
+end
+
+return M
