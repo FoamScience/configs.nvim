f1d8de9660a59de3c8190c5a641037d5c1ab2fc0
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2026-01-22 14:01:30 +0100

    fix: navbuddy moved repos

--- lua/user/navbuddy.lua
+++ lua/user/navbuddy.lua
@@ -1,7 +1,8 @@
 local M = {
-	"SmiteshP/nvim-navbuddy",
+	"hasansujon786/nvim-navbuddy",
 	event = "VeryLazy",
 	dependencies = {
+		"neovim/nvim-lspconfig",
 		"SmiteshP/nvim-navic",
 		"MunifTanjim/nui.nvim",
 	},
