ab998b0e1f841b6b44e98dbfd7f146e82bb20377
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2026-02-03 09:16:49 +0100

    fix: sane keymaps for outline.nvim

--- lua/user/outline.lua
+++ lua/user/outline.lua
@@ -6,7 +6,22 @@ local M = {
 
 function M.config()
 	require("outline").setup({
-
+		keymaps = {
+			show_help = '?',
+			close = {'<ESC>', 'q'},
+			goto_location = {'<Cr>', 'go'},
+			goto_and_close = '<S-Cr>',
+			peek_location = {'P'},
+			restore_location = '<C-g>',
+			hover_symbol = 'K',
+			toggle_preview = 'p',
+			rename_symbol = 'r',
+			code_actions = 'a',
+			fold_toggle = 'f',
+			fold_toggle_all = 'F',
+			down_and_jump = '<C-j>',
+			up_and_jump = '<C-k>',
+		}
 	})
 end
 
