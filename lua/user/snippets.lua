e0b55dbd9ba82d699a37dc2646a88eea521201c4
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2026-02-08 10:20:33 +0100

    fix: snippet field hopping with Tab instead of CR

--- lua/user/snippets.lua
+++ lua/user/snippets.lua
@@ -9,7 +9,26 @@ local M = {
 }
 
 M.config = function()
-    require("luasnip").setup{}
+    local ls = require("luasnip")
+    ls.setup{}
+
+    -- Tab/S-Tab to jump between snippet nodes (not Enter)
+    vim.keymap.set({ "i", "s" }, "<Tab>", function()
+        if ls.expand_or_jumpable() then
+            ls.expand_or_jump()
+        else
+            -- Fall back to normal tab
+            return "<Tab>"
+        end
+    end, { silent = true, expr = true })
+
+    vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+        if ls.jumpable(-1) then
+            ls.jump(-1)
+        else
+            return "<S-Tab>"
+        end
+    end, { silent = true, expr = true })
 end
 
 return M
