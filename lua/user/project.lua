0ffea66ecc1c5b45195a94144b14356b63007a4b
Elwardi <e.fadeli@ianus-simulation.de>
2026-03-29 21:07:37 +0200

    fix: switch to well maintained project.nvim

--- lua/user/project.lua
+++ lua/user/project.lua
@@ -1,15 +1,34 @@
 local M = {
-    "ahmedkhalf/project.nvim",
-    event = "VeryLazy",
+    'DrKJeff16/project.nvim',
+    dependencies = { 'folke/snacks.nvim', },
+    cmd = {
+        'Project',
+        'ProjectAdd',
+        'ProjectConfig',
+        'ProjectDelete',
+        'ProjectExport',
+        'ProjectFzf', -- If using `fzf-lua` integration
+        'ProjectHealth',
+        'ProjectHistory',
+        'ProjectImport',
+        'ProjectLog', -- If logging is enabled
+        'ProjectRecents',
+        'ProjectRoot',
+        'ProjectSession',
+        'ProjectSnacks', -- If using `snacks.nvim` integration
+    },
 }
 
 function M.config()
-    require("project_nvim").setup {
+    require("project").setup {
         active = true,
         on_config_done = nil,
         manual_mode = false,
         detection_methods = { "pattern" },
-        patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "pom.xml", "Make", "system" },
+        patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn",
+            "Makefile", "package.json", "pom.xml", "Make", "system",
+            ".pre-commit-config.yaml", ".pre-commit-config.yml", "pyproject.yaml", "pyproject.toml" },
+        snacks = { enabled = true },
         ignore_lsp = {},
         exclude_dirs = {},
         show_hidden = false,
